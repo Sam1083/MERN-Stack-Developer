f4393f66a8036b41a3e0db91ed4bf285936f6968
unknown <Sami Ullah Azeem>
2023-12-16 19:01:58 +0500

    code is udated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,14 +4,25 @@ const contact = () => {
 
 
 
-    // const user={ name :"sami", age:27,profession: "lawyer", married: false,children: "absolutly not" }= useState(object)
-
 
 
 
   return (
-    <div></div>
-    
+    <div className='d-flex justify-content-center align-items-center flex-column mt-4'>
+      <div class="mb-3 w-50">
+        <label for="exampleFormControlInput1" class="form-label">Name</label>
+        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="name" />
+      </div>
+      <div class="mb-3 w-50">
+        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email" />
+      </div>
+      <div class="mb-3 w-50">
+        <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Message" rows="3"></textarea>
+      </div>
+      <button className='btn btn-primary'>Submit</button>
+    </div>    
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,39 +5,35 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <Link class="navbar-brand" to="/">store</Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-      <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" to="/">Home</Link>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="/products">product</Link>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="/contact">contact</Link>
-        </li><li class="nav-item">
-          <Link class="nav-link" to="/about">about</Link>
-        </li><li class="nav-item">
-          <Link class="nav-link" to="/login">login</Link>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        {/* <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/> */}
-        <button class="btn btn-outline-success" type="submit">cart</button>
-      </form>
-    </div>
-  </div>
-</nav>
-
-
-
-
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <Link class="navbar-brand" to="/">store</Link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/products">product</Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/contact">contact</Link>
+            </li><li class="nav-item">
+              <Link class="nav-link" to="/about">about</Link>
+            </li><li class="nav-item">
+              <Link class="nav-link" to="/login">login</Link>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            {/* <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/> */}
+            <button class="btn btn-outline-success" type="submit">cart</button>
+          </form>
+        </div>
+      </div>
+    </nav>
   )
 }
 
